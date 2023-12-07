276a12453811c5c6b63c9fd56496dc1465164914
OptoCloud <26223094+OptoCloud@users.noreply.github.com>
2023-12-07 10:38:42 +0100

    Redirect to home page if logged in on root

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
+  import { browser } from '$app/environment';
+  import { goto } from '$app/navigation';
   import { PUBLIC_APP_NAME } from '$env/static/public';
+  import { AuthTokenStore } from '$lib/stores';
+
+  $: if (browser && !!$AuthTokenStore) {
+    goto('/home');
+  }
 </script>
 
 <div class="px-8 py-16">
